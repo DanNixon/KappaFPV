c9e3e2d34b0f283fa6a5c2dc025f0d36d04525b5
Dan Nixon <dan@dan-nixon.com>
2017-01-07 19:20:14 +0000

    Fix title card

--- shotcut/title_animation.js
+++ shotcut/title_animation.js
@@ -1,5 +1,4 @@
 function clamped_map(value, in_low, in_high, out_low, out_high) {
-  console.log("map, v=" + value);
   if (value <= in_low)
     return out_low;
   if (value >= in_high)
@@ -14,27 +13,19 @@ function Transition(logoDiv, titleDiv) {
 }
 
 Transition.prototype.render = function (time) {
-  var mainOpacity = 1 - clamped_max(time, 0.6, 1, 0, 1);
+  var titleOpacity = clamped_map(time, 0.8, 1, 1, 0);
 
-  this.logoDiv.style.borderRightColor = "rgba(157, 151, 217, " + mainOpacity + ")";
-  this.logoDiv.style.opacity = mainOpacity;
+  this.logoDiv.style.borderRightColor = "rgba(157, 151, 217, " + titleOpacity + ")";
+  this.logoDiv.style.opacity = titleOpacity;
 
   this.titleDiv.style.top = clamped_map(time, 0.02, 0.05, 250, 0) + "px";
-  this.titleDiv.style.opacity = mainOpacity;
+  this.titleDiv.style.opacity = titleOpacity;
 }
 
 function onLoad() {
-  onResize();
-
   var transition = new Transition(document.getElementById("logo"), document.getElementById("video_title"));
-
   webvfx.renderRequested.connect(transition, Transition.prototype.render);
-
   webvfx.readyRender(true)
 }
 
-function onResize() {
-}
-
 window.addEventListener("load", onLoad, false);
-window.addEventListener("load", onResize, false);
